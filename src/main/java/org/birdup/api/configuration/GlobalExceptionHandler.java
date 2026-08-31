1328df4bd151a15e939592f3cc9ffd011e014799
Layne <pierce77@bu.edu>
2026-08-31 11:18:08 -0400

    Update `GlobalExceptionHandler` to use base class `ResponseEntityExceptionHandler`

--- src/main/java/org/birdup/api/configuration/GlobalExceptionHandler.java
+++ src/main/java/org/birdup/api/configuration/GlobalExceptionHandler.java
@@ -1,21 +1,29 @@
 package org.birdup.api.configuration;
 
+import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     /// Handles exceptions thrown for request bodies that do not
     /// pass validation constraints.
     @ExceptionHandler({
             MethodArgumentNotValidException.class
     })
-    public ResponseEntity<HttpStatus> handleValidationError() {
+    public ResponseEntity<HttpStatus> handleValidationError(Exception ex, WebRequest request) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
     }
 
+    /*@ExceptionHandler({
+            ChangeSetPersister.NotFoundException.class
+    })
+    public ResponseEntity<HttpStatus> handleNotFoundError(Exception ex) {
+    }*/
 }
