9234e7686e3e7d5c8aac4f376cd06214c8c83f25
Layne <pierce77@bu.edu>
2026-09-15 21:25:09 -0400

    Update individual sighting card aesthetics

--- birdup-web/src/app/components/sighting-card/sighting-card.ts
+++ birdup-web/src/app/components/sighting-card/sighting-card.ts
@@ -1,20 +1,14 @@
-import { Component, input } from '@angular/core';
-import {TuiIcon, TuiTitle} from '@taiga-ui/core';
+import { Component, input, ChangeDetectionStrategy } from '@angular/core';
+import { TuiIcon, TuiTitle } from '@taiga-ui/core';
 import { TuiBadge } from '@taiga-ui/kit';
-import {TuiCardLarge, TuiCardRow, TuiHeader} from "@taiga-ui/layout";
+import { TuiCardLarge, TuiCardRow, TuiHeader } from '@taiga-ui/layout';
 import Sighting from '../../services/sighting.models';
 
 @Component({
   selector: 'app-sighting-card',
-  imports: [
-    TuiBadge,
-    TuiIcon,
-    TuiTitle,
-    TuiCardRow,
-    TuiCardLarge,
-    TuiHeader,
-  ],
+  imports: [TuiBadge, TuiIcon, TuiTitle, TuiCardRow, TuiCardLarge, TuiHeader],
   templateUrl: './sighting-card.html',
+  changeDetection: ChangeDetectionStrategy.Eager,
   styleUrl: './sighting-card.less',
 })
 export class SightingCard {
